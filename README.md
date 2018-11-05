70cd9531892099ccd7628ffd7929335b6963d554
Ahmed Kamel <ahmedk@zadgroup.net>
2018-11-05 14:54:45 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# How to run
+```bash
+npm install
+ROOT_PATH="secret/tenant/zadgroup/websites/"
+npx run delete_path "${ROOT_PATH}"
+```
--- runfile.js
+++ runfile.js
@@ -1,8 +1,20 @@
 const { run } = require('runjs');
 
-function delete_path(dir_path) {
-    const first_level= run(`vault kv list ${dir_path}`, {stdio: "pipe"});
-    console.log(first_level);
+function delete_path(root_path) {
+    const child_paths = JSON.parse(run(`vault kv list -format json ${root_path}`, {stdio: "pipe"}));
+    for (const child_path of child_paths) {
+        try {
+          run(`vault kv get -format json ${root_path}${child_path}`);
+          run(`vault kv delete -versions=1 ${root_path}${child_path}`);
+          run(`vault kv destroy -versions=1 ${root_path}${child_path}`);
+          run(`vault kv metadata delete ${root_path}${child_path}`);
+        } catch (e) {
+          delete_path(`${root_path}${child_path}`);
+          run(`vault kv delete -versions=1 ${root_path}${child_path}`);
+          run(`vault kv destroy -versions=1 ${root_path}${child_path}`);
+          run(`vault kv metadata delete ${root_path}${child_path}`);
+        }
+    }
 }
 
 module.exports = {delete_path};
